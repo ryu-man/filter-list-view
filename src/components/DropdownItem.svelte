be53e8ca7d0770df64a1f05f3508346432868785
ryu-man <rioman19@gmail.com>
2021-05-15 11:44:44 +0100

    Rename variable

--- src/components/DropdownItem.svelte
+++ src/components/DropdownItem.svelte
@@ -5,19 +5,19 @@
 	const dispatch = createEventDispatcher();
 
 	const show = getContext(menuStatus);
-	const selectedItem = getContext(selected);
+	const selectionStore = getContext(selected);
 
 	export let value = "";
 </script>
 
 <div
 	class="text-gray-700 block px-4 py-2 text-sm  cursor-pointer hover:bg-gray-100"
-    class:text-gray-900={value === $selectedItem}
-	class:font-bold={value === $selectedItem}
+    class:text-gray-900={value === $selectionStore}
+	class:font-bold={value === $selectionStore}
 	role="menuitem"
 	tabindex="-1"
 	on:click|preventDefault={(e) => {
-		$selectedItem = value;
+		$selectionStore = value;
 		$show = false;
 		dispatch("click");
 	}}
