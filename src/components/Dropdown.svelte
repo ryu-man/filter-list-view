520d29ff6c9cfb87f7024da1e5381db5d79de59f
ryu-man <rioman19@gmail.com>
2021-05-16 21:56:08 +0100

    Set max height

--- src/components/Dropdown.svelte
+++ src/components/Dropdown.svelte
@@ -99,7 +99,7 @@
 	-->
 	{#if $show}
 		<div
-			class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none z-10 w-full"
+			class="dropdown-menu origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none z-10 w-full"
 			role="menu"
 			aria-orientation="vertical"
 			aria-labelledby="menu-button"
@@ -110,3 +110,10 @@
 		</div>
 	{/if}
 </div>
+
+<style>
+	.dropdown-menu {
+		max-height: calc(8px + 6 * 36px);
+		overflow: overlay;
+	}
+</style>
